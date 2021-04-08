e94f2baea172e5065c6791718635fb034a791d2f
naren67 <naren678n@gmail.com>
2021-04-08 21:11:35 +0530

    first commit

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+          apiKey: "AIzaSyDNqkYNm4-D2qzUyQEKWJ28OPTBylIf80w",
+          authDomain: "tinder-59045.firebaseapp.com",
+          projectId: "tinder-59045",
+          storageBucket: "tinder-59045.appspot.com",
+          messagingSenderId: "799130835254",
+          appId: "1:799130835254:web:c608046c77383ef4cc7b97",
+          measurementId: "G-M7K5GC5236"
+        };
